edaf8a7ecfb611632538175f2df67c42572110a9
Chetlapalle Vishnu Prabhav <ee25b035@smail.iitm.ac.in>
2025-09-05 16:36:40 +0530

    Update Q6(B).py

--- Assignment1/Code Files/Q6(B).py
+++ Assignment1/Code Files/Q6(B).py
@@ -1,21 +1,11 @@
-x=input("Enter the string:")
-#Taking a random string
-list1=[]
-#One list to store characters which are read already
-list2=[]
-#One list to store characters and frequency
+x=input("Enter the string: ")
+x=x.lower()#removing anomalies like lower and upper cases
+x=x.strip()#removing spaces, which are usually counted by python as separate element
+#Creating empty dictionary  
+dict={}#empty dictionary
 for ch in x:
-    if ch in list1:
-        continue
+    if ch in dict:
+        dict[ch]+=1
     else:
-        list1.append(ch)
-#Build list with all distinct elements to compare it later        
-for element in list1:
-    times=0
-    for ch in x:
-        if ch==element:
-            times+=1
-    list2.append((element,times))
-#Build list with frequency also along with the element itself           
-print(list2)  
-#2 for loops right
+        dict[ch]=1
+print(dict)
