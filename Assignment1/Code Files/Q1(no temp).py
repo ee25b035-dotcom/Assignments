b7c1bb703e4fade8ccfb42cf82ab0c5d504025b2
Chetlapalle Vishnu Prabhav <ee25b035@smail.iitm.ac.in>
2025-09-05 14:31:59 +0530

    Add Q1(no temp).py for swapping values without temp

---
+++ Assignment1/Code Files/Q1(no temp).py
@@ -0,0 +1,18 @@
+ 1. a=5
+ 2. #Initialise a
+ 3. print(a)
+ 4. #printing a for reference
+ 5. b=10
+ 6. #initialise b
+ 7. print(b)
+ 8. #print b for reference
+ 9. a=a+b
+10. #using without temp by storing the sum
+11. b=a-b
+12. #retrieving value of a
+13. a=a-b
+14. #retrieving value of b
+15. print(a)
+16. #printing a
+print(b)
+#printing b
